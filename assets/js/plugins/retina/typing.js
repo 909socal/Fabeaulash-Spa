cd284e863bfbf28afe2694608a56e51d40dade26
Gurjot Singh <celica09@gmail.com>
2016-05-16 12:04:42 -0700

    online commit

---
+++ assets/js/plugins/retina/typing.js
@@ -0,0 +1,22 @@
+'use strict'
+$(document).ready(function() {
+	$(function() {
+		$("#typing").typed({
+			strings: ["Skin Care. ^800",
+			"Lash and Brows. ^800",
+			"Lash Extensions. ^800",
+			"Four Layer Facials. ^800",
+			"Brightening Treatment. ^800",
+			"Artesian Seaweed Wax. ^800",
+			"Body Slimming. ^800",
+			"Acne &amp; Oily Skin Treatment. ^800",
+			"Anti-Stress Facials. ^800",
+			"Eye Contour Treatment. ^800",
+			"Brow Extensions. ^800",
+			"Lash and Brow Shaping. ^800" ],
+			typeSpeed: 120,
+			backSpeed: 90
+		});
+	});
+	
+});
